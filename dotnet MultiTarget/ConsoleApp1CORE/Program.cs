d30a4d8a660923c96369b9d4e78399a71628296e
António Campos <jantoniofcampos@sapo.pt>
2019-03-29 18:15:21 +0000

    dotnet Multi target

---
+++ dotnet MultiTarget/ConsoleApp1CORE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1CORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibExemplo.Class1 class1 = new LibExemplo.Class1();
+            Console.WriteLine(class1.TST);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dotnet MultiTarget/LibExemplo/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace LibExemplo
+{
+    public class Class1
+    {
+        public string TST;
+        public Class1()
+        {
+#if NETFULL
+            TST = "NETFULL";
+#endif
+
+#if NETSTANDARD
+            TST = "NETSTANDARD";
+#endif
+        }
+    }
+}
